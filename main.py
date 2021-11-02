fcea58978a88bc10b7f09e5703e8d660e2fd3ca4
ilimski <73356431+ilimski@users.noreply.github.com>
2021-11-03 01:57:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -292,6 +292,8 @@ def WorldNews9(message):
         bot.register_next_step_handler(message, WorldNews6)
         bot.register_next_step_handler(message, WorldNews7)
         bot.register_next_step_handler(message, WorldNews8)
+        
+bot.polling(none_stop=True)        
 
 def main():
     bot.polling(none_stop=True)
